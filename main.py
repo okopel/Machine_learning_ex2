0b728d1cfa5cbf98310df7f35c248cbaf68b2790
okopel <okopel@gmail.com>
2019-05-12 16:28:07 +0300

    v1.0

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import numpy as np
-import scipy as scipy
-
-from perceptron import perceptron
+from scipy import stats
 
 
 def Z_normalize(arrOfParams):
     # return (arrOfParams - np.mean) / np.std(np.divide())
-    return scipy.stats.mstats.zscore(arrOfParams)
+    print("norm\n")
+    print(stats.zscore(arrOfParams))
+    return stats.zscore(arrOfParams)
+
+
+def MinMax_normalize(arrOfParams):
+    # (x-min)/(max-min)
+    for i in range(len(arrOfParams[0])):
+        minArg = float(min(arrOfParams[:, i]))
+        maxArg = float(max(arrOfParams[:, i]))
+        if minArg == maxArg:  # todo
+            return
+        for j in range(len(arrOfParams)):
+            old = arrOfParams[j, i]
+            arrOfParams[j, i] = (float(arrOfParams[j, i]) - minArg) / (maxArg - minArg)
+        return arrOfParams
 
 
 # if __name__ == '__main__':
 def main():
     # todo argv[0]
-    # , dtype="|U5"
+    # read the training set
     X = np.genfromtxt("train_x.txt", delimiter=',', dtype="|U5")
     X[X == 'M'] = 0
     X[X == 'F'] = 1
     X[X == 'I'] = 2
-    Y = np.genfromtxt("train_y.txt", delimiter=",", dtype="|U5")
+    Y = np.genfromtxt("train_y.txt", delimiter=",")
     print(X, Y)
-    X = Z_normalize(X)
-    print("after norm\n")
+    # X = Z_normalize(X)
+    X = MinMax_normalize(X)
     print(X, Y)
-    perceptron(X, Y)
+    # perceptron(X, Y)
 
 
 main()
