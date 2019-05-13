4bcac387452358851607910dcf469a5ef4e17c21
okopel <okopel@gmail.com>
2019-05-13 22:11:06 +0300

    v1.8

--- perceptron.py
+++ perceptron.py
@@ -4,49 +4,34 @@ Shlomo Rabinovich 308432517 rabinos
 """
 
 import numpy as np
-from sklearn.utils import shuffle
 
 
-def perceptron(X_train: np, Y_train: np) -> object:
-    # num of args in the training set
-    m = len(X_train)
-    # num of args in one example
-    d = len(X_train[0])
-    # the change parameter
-    eta = 0.25
-    # vector of 2D (0,0,0,0...,0),(0,0,0,0...,0),(0,0,0,0...,0)..
-    w = np.zeros((3, d))
+class Perceptron:
+    def __init__(self, t_data, t_label, clssesNum, eta=0.25, epochs=50):
+        self.t_data = t_data  # data of training set
+        self.t_label = t_label  # label of training set
+        # self.n = len(t_data)  # set size
+        self.m = len(t_data[0])  # parameters of one entry
+        self.eta = eta
+        self.epochs = epochs  # how many iterats
+        self.classesNum = clssesNum  # how many multy class
 
-    # <<Perceptron>>
-    # multi class
-    # num of iterations
-    epochs = 50
-    for e in range(epochs):
-        # take one random example from the training set
-        X_train, Y_train = shuffle2np(X_train, Y_train)
-        # x= out example while y is the class
-        for x, y in zip(X_train, Y_train):
-            x = x.astype(np.float)
-            # predict
-            # y_hat take the index of the max arg of multiply between x and w
-            # w = hypnotize
-            y_hat = np.argmax(np.dot(w, x))
-            # update
-            # check if y(the real class) != our class calculate
-            if y != y_hat:
-                # add little change to all the parameters in the wrong hypnotize to make more distance
-                # w[y, :] += eta * x
-                etax = eta * np.array(x)
-                w[int(float(y))] += etax
-                # make the true hypnotize closer to us
-                # w[y_hat, :] -= eta * x
-                w[int(float(y_hat))] -= etax
-    return w
+    def train(self):
+        w = np.zeros((self.classesNum, self.m))
+        for e in range(self.epochs):
+            self.t_data, self.t_label = shuffle2np(self.t_data, self.t_label)
+            for x, y in zip(self.t_data, self.t_label):
+                y_hat = np.argmax(np.dot(w, x))
+                if y != y_hat:
+                    etax = self.eta * np.array(x)
+                    w[int(y)] += etax
+                    w[int(y_hat)] -= etax
+        return w
 
 
-def shuffle2np(x, y):
+def shuffle2np(x, y):  # todo is it work?
     ret = np.c_[x, y]
-    ret = shuffle(ret)
+    ret = np.random.mtrand.shuffle(ret)
     x = ret[:, :-1]
     y = ret[:, -1]
     return x, y
